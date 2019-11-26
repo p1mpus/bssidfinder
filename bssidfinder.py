7e4578f7f7f3ea7fc696c626f66f0ef235a9274d
p1mpus <45327810+p1mpus@users.noreply.github.com>
2019-11-26 14:02:09 +0300

    Add files via upload

---
+++ bssidfinder.py
@@ -0,0 +1,13 @@
+import requests, os
+
+os.system('clear')
+print("<//LYNEX TEAM//>")
+print("[BSSIDFinder / coded by p1mpus]")
+
+bssid = input("Enter BSSID: ")
+print("")
+ss = "http://mobile.maps.yandex.net/cellid_location/?clid=1866854&lac=-1&cellid=-1&operatorid=null&countrycode=null&signalstrength=-1&wifinetworks=" + bssid + ":-65&app=ymetro"
+url = ss
+r = requests.get(url)
+print(r.text)
+input("Enter...)
